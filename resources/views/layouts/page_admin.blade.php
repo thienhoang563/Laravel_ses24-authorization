263bfabce9b1e5a2a026da3149f5f307fa5ae486
thienhoang563 <thienhoang563@gmail.com>
2019-03-04 14:00:29 +0700

    Authorization

---
+++ resources/views/layouts/page_admin.blade.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thienhmp
+ * Date: 04/03/2019
+ * Time: 11:25
+ */
+echo "Hello admin";
---
+++ resources/views/layouts/page_guest.blade.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thienhmp
+ * Date: 04/03/2019
+ * Time: 11:24
+ */
+
+echo "Hello guest";
